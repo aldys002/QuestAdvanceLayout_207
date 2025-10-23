05facc1d6ae489d8e016461e88bae56371d755f9
aldys002 <igidiaaldys@gmail.com>
2025-10-23 20:33:58 +0700

    Menambahkan Spacer

--- app/src/main/java/com/example/uipertama/UIPertama.kt
+++ app/src/main/java/com/example/uipertama/UIPertama.kt
@@ -93,6 +93,7 @@ fun ActivitasPertama(modifier: Modifier){
                     .size(size = 100.dp)
                     .padding(all = 5.dp)
             )
+            Spacer(modifier = Modifier.width(width = 30.dp))
         }
     }
 }
